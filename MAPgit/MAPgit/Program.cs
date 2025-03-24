b439b3b42e3656543e9a9532b97f0062ebfc17a9
victorcastro <viccas03@ucm.es>
2025-03-24 11:33:52 +0100

    Archivos iniciales añadidos al repositorio

---
+++ MAPgit/MAPgit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MAPgit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Holis este programa es muy complejo y tiene muchas cositas
+        }
+    }
+}
